0be0e3710076c9360ecda8f817f66ae183ff43b2
Devansh Raghuwanshi <151429360+sigmadev92@users.noreply.github.com>
2025-08-26 08:41:03 +0530

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,16 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  // Config your firebase here
-  apiKey: "AIzaSyBEpVJgkkXjtUs2xGImtEev7F1-p8Gog2M",
-  authDomain: "busy-by-1.firebaseapp.com",
-  projectId: "busy-by-1",
-  storageBucket: "busy-by-1.firebasestorage.app",
-  messagingSenderId: "337830709974",
-  appId: "1:337830709974:web:02f996f8a16d1762a89777",
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-export { db };
